608919849918c99549aac778d28a4493163e7ab5
omni-axa <kgoon007@gmail.com>
2011-01-09 22:53:32 +0530

    BG2Tweaks amulet of prot +1

--- cespy/c2amul01.d
+++ cespy/c2amul01.d
@@ -11,11 +11,24 @@ APPEND BOTSMITH
 			PartyHasItem("amul24")
 			PartyHasItem("amul25")
 			NumItemsPartyGT("amul14",1)~ THEN GOTO XO#ItemUp02
-		IF ~OR(4)
+		IF ~PartyHasItem("amul22")
+			PartyHasItem("amul24")
+			PartyHasItem("amul25")
+			NumItemsPartyGT("cdamul14",1)~ THEN GOTO XO#ItemUp03
+		IF ~PartyHasItem("amul22")
+			PartyHasItem("amul24")
+			PartyHasItem("amul25")
+			NumItemsPartyGT("amul14cd",1)~ THEN GOTO XO#ItemUp04
+		IF ~PartyHasItem("amul22")
+			PartyHasItem("amul24")
+			PartyHasItem("amul25")
+			NumItemsPartyLT("amul14",2)
+			NumItemsPartyLT("cdamul14",2)
+			NumItemsPartyLT("amul14cd",2)~ THEN GOTO XO#ItemUp01
+		IF ~OR(3)
 				!PartyHasItem("amul22")
 				!PartyHasItem("amul24")
-				!PartyHasItem("amul25")
-				NumItemsPartyLT("amul14",2)~ THEN GOTO XO#ItemUp01
+				!PartyHasItem("amul25")~ THEN GOTO XO#ItemUp01
 	END
   
 	IF ~~ THEN BEGIN XO#ItemUp01 SAY @5678887
@@ -39,6 +52,46 @@ APPEND BOTSMITH
 													 DestroyItem("amul14")
 													 TakePartyItemNum("amul14",1)
 													 DestroyItem("amul14")~ GOTO 11
+    IF ~~ THEN REPLY #66770 GOTO XO#NoItemUp
+	END
+  
+	IF ~~ THEN BEGIN XO#ItemUp03 SAY @5678987
+		IF ~PartyGoldLT(10000)~ THEN REPLY #66662 GOTO XO#NoItemUp
+		IF ~PartyGoldGT(9999)~ THEN REPLY #66664 DO ~SetGlobal("XO#TOBItemUp","GLOBAL",35)
+											 		 SetGlobal("ImpForgeStuff","GLOBAL",1)
+													 SetGlobal("XO#TOBCraft","GLOBAL",1)
+													 TakePartyGold(10000)
+													 DestroyGold(10000)
+													 TakePartyItemNum("amul22",1)
+													 DestroyItem("amul22")
+													 TakePartyItemNum("amul24",1)
+													 DestroyItem("amul24")
+													 TakePartyItemNum("amul25",1)
+													 DestroyItem("amul25")
+													 TakePartyItemNum("cdamul14",1)
+													 DestroyItem("cdamul14")
+													 TakePartyItemNum("cdamul14",1)
+													 DestroyItem("cdamul14")~ GOTO 11
+    IF ~~ THEN REPLY #66770 GOTO XO#NoItemUp
+	END
+  
+	IF ~~ THEN BEGIN XO#ItemUp04 SAY @5678987
+		IF ~PartyGoldLT(10000)~ THEN REPLY #66662 GOTO XO#NoItemUp
+		IF ~PartyGoldGT(9999)~ THEN REPLY #66664 DO ~SetGlobal("XO#TOBItemUp","GLOBAL",35)
+											 		 SetGlobal("ImpForgeStuff","GLOBAL",1)
+													 SetGlobal("XO#TOBCraft","GLOBAL",1)
+													 TakePartyGold(10000)
+													 DestroyGold(10000)
+													 TakePartyItemNum("amul22",1)
+													 DestroyItem("amul22")
+													 TakePartyItemNum("amul24",1)
+													 DestroyItem("amul24")
+													 TakePartyItemNum("amul25",1)
+													 DestroyItem("amul25")
+													 TakePartyItemNum("amul14cd",1)
+													 DestroyItem("amul14cd")
+													 TakePartyItemNum("amul14cd",1)
+													 DestroyItem("amul14cd")~ GOTO 11
     IF ~~ THEN REPLY #66770 GOTO XO#NoItemUp
 	END
 
--- crom/c2amul01.d
+++ crom/c2amul01.d
@@ -11,11 +11,24 @@ APPEND WSMITH01
 			PartyHasItem("amul24")
 			PartyHasItem("amul25")
 			NumItemsPartyGT("amul14",1)~ THEN GOTO XO#ItemUp02
-		IF ~OR(4)
+		IF ~PartyHasItem("amul22")
+			PartyHasItem("amul24")
+			PartyHasItem("amul25")
+			NumItemsPartyGT("cdamul14",1)~ THEN GOTO XO#ItemUp03
+		IF ~PartyHasItem("amul22")
+			PartyHasItem("amul24")
+			PartyHasItem("amul25")
+			NumItemsPartyGT("amul14cd",1)~ THEN GOTO XO#ItemUp04
+		IF ~PartyHasItem("amul22")
+			PartyHasItem("amul24")
+			PartyHasItem("amul25")
+			NumItemsPartyLT("amul14",2)
+			NumItemsPartyLT("cdamul14",2)
+			NumItemsPartyLT("amul14cd",2)~ THEN GOTO XO#ItemUp01
+		IF ~OR(3)
 				!PartyHasItem("amul22")
 				!PartyHasItem("amul24")
-				!PartyHasItem("amul25")
-				NumItemsPartyLT("amul14",2)~ THEN GOTO XO#ItemUp01
+				!PartyHasItem("amul25")~ THEN GOTO XO#ItemUp01
 	END
   
 	IF ~~ THEN BEGIN XO#ItemUp01 SAY @1234878
@@ -38,6 +51,44 @@ APPEND WSMITH01
 													 DestroyItem("amul14")
 													 TakePartyItemNum("amul14",1)
 													 DestroyItem("amul14")~ GOTO 56
+    IF ~~ THEN REPLY #66770 GOTO XO#NoItemUp
+	END
+  
+	IF ~~ THEN BEGIN XO#ItemUp02 SAY @1234978
+		IF ~PartyGoldLT(10000)~ THEN REPLY #66662 GOTO XO#NoItemUp
+		IF ~PartyGoldGT(9999)~ THEN REPLY #66664 DO ~SetGlobal("XO#ItemUp","ar0334",35)
+											 		 SetGlobal("ForgeStuff","GLOBAL",1)
+													 TakePartyGold(10000)
+													 DestroyGold(10000)
+													 TakePartyItemNum("amul22",1)
+													 DestroyItem("amul22")
+													 TakePartyItemNum("amul24",1)
+													 DestroyItem("amul24")
+													 TakePartyItemNum("amul25",1)
+													 DestroyItem("amul25")
+													 TakePartyItemNum("cdamul14",1)
+													 DestroyItem("cdamul14")
+													 TakePartyItemNum("cdamul14",1)
+													 DestroyItem("cdamul14")~ GOTO 56
+    IF ~~ THEN REPLY #66770 GOTO XO#NoItemUp
+	END
+  
+	IF ~~ THEN BEGIN XO#ItemUp02 SAY @1234978
+		IF ~PartyGoldLT(10000)~ THEN REPLY #66662 GOTO XO#NoItemUp
+		IF ~PartyGoldGT(9999)~ THEN REPLY #66664 DO ~SetGlobal("XO#ItemUp","ar0334",35)
+											 		 SetGlobal("ForgeStuff","GLOBAL",1)
+													 TakePartyGold(10000)
+													 DestroyGold(10000)
+													 TakePartyItemNum("amul22",1)
+													 DestroyItem("amul22")
+													 TakePartyItemNum("amul24",1)
+													 DestroyItem("amul24")
+													 TakePartyItemNum("amul25",1)
+													 DestroyItem("amul25")
+													 TakePartyItemNum("amul14cd",1)
+													 DestroyItem("amul14cd")
+													 TakePartyItemNum("amul14cd",1)
+													 DestroyItem("amul14cd")~ GOTO 56
     IF ~~ THEN REPLY #66770 GOTO XO#NoItemUp
 	END
 
