46de074bacad1dfce730f3d6281251b0b2173a4b
omni-axa <kgoon007@gmail.com>
2011-01-09 23:22:20 +0530

    BG2Tweaks ring of prot +2

--- cespy/c2ring02.d
+++ cespy/c2ring02.d
@@ -1,16 +1,24 @@
 EXTEND_BOTTOM BOTSMITH 4
-	IF ~PartyHasItem("ring07")~ THEN GOTO XO#ItemUp00
+	IF ~OR(3)
+			PartyHasItem("ring07")
+			PartyHasItem("cdring07")
+			PartyHasItem("ring07cd")~ THEN GOTO XO#ItemUp00
 END
   
 APPEND BOTSMITH
 	IF ~~ THEN BEGIN XO#ItemUp00 SAY @567841
-		IF ~PartyHasItem("ring07")
-			OR(2)
+		IF ~OR(2)
 				!PartyHasItem("ring31")
 				!PartyHasItem("misc42")~ THEN GOTO XO#ItemUp01
 		IF ~PartyHasItem("ring07")
 			PartyHasItem("ring31")
 			PartyHasItem("misc42")~ THEN GOTO XO#ItemUp02
+		IF ~PartyHasItem("cdring07")
+			PartyHasItem("ring31")
+			PartyHasItem("misc42")~ THEN GOTO XO#ItemUp03
+		IF ~PartyHasItem("ring07cd")
+			PartyHasItem("ring31")
+			PartyHasItem("misc42")~ THEN GOTO XO#ItemUp04
 	END
   
 	IF ~~ THEN BEGIN XO#ItemUp01 SAY @5678841
@@ -26,6 +34,40 @@ APPEND BOTSMITH
 													 DestroyGold(20000)
 													 TakePartyItemNum("ring07",1)
 													 DestroyItem("ring07")
+													 TakePartyItemNum("ring31",1)
+													 DestroyItem("ring31")
+													 TakePartyItemNum("misc42",1)
+													 DestroyItem("misc42")~ GOTO 11
+    IF ~~ THEN REPLY #66770 GOTO XO#NoItemUp
+	END
+
+  
+	IF ~~ THEN BEGIN XO#ItemUp03 SAY @5678941
+		IF ~PartyGoldLT(20000)~ THEN REPLY #66662 GOTO XO#NoItemUp
+		IF ~PartyGoldGT(19999)~ THEN REPLY #66664 DO ~SetGlobal("XO#TOBItemUp","GLOBAL",2)
+											 		 SetGlobal("ImpForgeStuff","GLOBAL",1)
+													 SetGlobal("XO#TOBCraft","GLOBAL",1)
+													 TakePartyGold(20000)
+													 DestroyGold(20000)
+													 TakePartyItemNum("cdring07",1)
+													 DestroyItem("cdring07")
+													 TakePartyItemNum("ring31",1)
+													 DestroyItem("ring31")
+													 TakePartyItemNum("misc42",1)
+													 DestroyItem("misc42")~ GOTO 11
+    IF ~~ THEN REPLY #66770 GOTO XO#NoItemUp
+	END
+
+  
+	IF ~~ THEN BEGIN XO#ItemUp04 SAY @5678941
+		IF ~PartyGoldLT(20000)~ THEN REPLY #66662 GOTO XO#NoItemUp
+		IF ~PartyGoldGT(19999)~ THEN REPLY #66664 DO ~SetGlobal("XO#TOBItemUp","GLOBAL",2)
+											 		 SetGlobal("ImpForgeStuff","GLOBAL",1)
+													 SetGlobal("XO#TOBCraft","GLOBAL",1)
+													 TakePartyGold(20000)
+													 DestroyGold(20000)
+													 TakePartyItemNum("ring07cd",1)
+													 DestroyItem("ring07cd")
 													 TakePartyItemNum("ring31",1)
 													 DestroyItem("ring31")
 													 TakePartyItemNum("misc42",1)
--- crom/c2ring02.d
+++ crom/c2ring02.d
@@ -1,16 +1,24 @@
 EXTEND_BOTTOM WSMITH01 13
-	IF ~PartyHasItem("ring07")~ THEN GOTO XO#ItemUp00
+	IF ~OR(3)
+			PartyHasItem("ring07")
+			PartyHasItem("cdring07")
+			PartyHasItem("ring07cd")~ THEN GOTO XO#ItemUp00
 END
   
 APPEND WSMITH01
 	IF ~~ THEN BEGIN XO#ItemUp00 SAY @123441
-		IF ~PartyHasItem("ring07")
-			OR(2)
+		IF ~OR(2)
 				!PartyHasItem("ring31")
 				!PartyHasItem("misc42")~ THEN GOTO XO#ItemUp01
 		IF ~PartyHasItem("ring07")
 			PartyHasItem("ring31")
 			PartyHasItem("misc42")~ THEN GOTO XO#ItemUp02
+		IF ~PartyHasItem("cdring07")
+			PartyHasItem("ring31")
+			PartyHasItem("misc42")~ THEN GOTO XO#ItemUp03
+		IF ~PartyHasItem("ring07cd")
+			PartyHasItem("ring31")
+			PartyHasItem("misc42")~ THEN GOTO XO#ItemUp04
 	END
   
 	IF ~~ THEN BEGIN XO#ItemUp01 SAY @1234841
@@ -25,6 +33,36 @@ APPEND WSMITH01
 													 DestroyGold(20000)
 													 TakePartyItemNum("ring07",1)
 													 DestroyItem("ring07")
+													 TakePartyItemNum("ring31",1)
+													 DestroyItem("ring31")
+													 TakePartyItemNum("misc42",1)
+													 DestroyItem("misc42")~ GOTO 56
+    IF ~~ THEN REPLY #66770 GOTO XO#NoItemUp
+	END
+  
+	IF ~~ THEN BEGIN XO#ItemUp03 SAY @1234941
+		IF ~PartyGoldLT(20000)~ THEN REPLY #66662 GOTO XO#NoItemUp
+		IF ~PartyGoldGT(19999)~ THEN REPLY #66664 DO ~SetGlobal("XO#ItemUp","ar0334",2)
+											 		 SetGlobal("ForgeStuff","GLOBAL",1)
+													 TakePartyGold(20000)
+													 DestroyGold(20000)
+													 TakePartyItemNum("cdring07",1)
+													 DestroyItem("cdring07")
+													 TakePartyItemNum("ring31",1)
+													 DestroyItem("ring31")
+													 TakePartyItemNum("misc42",1)
+													 DestroyItem("misc42")~ GOTO 56
+    IF ~~ THEN REPLY #66770 GOTO XO#NoItemUp
+	END
+  
+	IF ~~ THEN BEGIN XO#ItemUp04 SAY @1234941
+		IF ~PartyGoldLT(20000)~ THEN REPLY #66662 GOTO XO#NoItemUp
+		IF ~PartyGoldGT(19999)~ THEN REPLY #66664 DO ~SetGlobal("XO#ItemUp","ar0334",2)
+											 		 SetGlobal("ForgeStuff","GLOBAL",1)
+													 TakePartyGold(20000)
+													 DestroyGold(20000)
+													 TakePartyItemNum("ring07cd",1)
+													 DestroyItem("ring07cd")
 													 TakePartyItemNum("ring31",1)
 													 DestroyItem("ring31")
 													 TakePartyItemNum("misc42",1)
